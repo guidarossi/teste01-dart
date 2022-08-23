1ce1b111dc4c8a35f4cfdf7e078a4229e121bbe7
GUILHERME FELIPE DAROSSI <63159108+guidarossi@users.noreply.github.com>
2022-08-23 17:12:10 -0300

    Update main.dart

--- test01-dart/main.dart
+++ test01-dart/main.dart
@@ -2,22 +2,28 @@ import 'dart:io';
 
 void main() {
   print("Digite um numero inteiro");
+  verificaEntrada(stdin.readLineSync()!);
+  print("Encerrando programa..");
+}
 
+void verificaEntrada(String num) {
   try {
-    var num = int.parse(stdin.readLineSync()!);
-
-    if (num.runtimeType == int) {
-      var result = 0;
-
-      for (int i = 0; i < num; i++) {
-        if (i % 3 == 0 || i % 5 == 0) {
-          result += i;
-        }
-      }
-      print("Resultado: " + result.toString());
-    }
+    int convert = int.parse(num);
+    operacao(convert);
   } on FormatException {
     print("Valor invalido!");
   }
-  print("Encerrando programa..");
+}
+
+void operacao(int valor) {
+  if (valor.runtimeType == int) {
+    var result = 0;
+
+    for (int i = 0; i < valor; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        result += i;
+      }
+    }
+    print("Resultado: " + result.toString());
+  }
 }
